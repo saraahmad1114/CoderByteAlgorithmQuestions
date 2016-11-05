8afed25b970bb5a9d455865f0dcabeae8317b1b5
Sara <saraahmad1114@yahoo.com>
2016-11-05 19:14:11 -0400

    first question

--- CoderByteAlgorithmQuestions.playground/Contents.swift
+++ CoderByteAlgorithmQuestions.playground/Contents.swift
@@ -3,3 +3,19 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+//Questions: How to reverse a string in swift? 
+
+func FirstReverse(str: String) -> String {
+    
+    var newString = str
+    var characters = newString.characters
+    var reverseCharacters = characters.reverse()
+    var returnString = String(reverseCharacters)
+    // Note: feel free to modify the return type of this function
+    return returnString
+    
+}
+
+
